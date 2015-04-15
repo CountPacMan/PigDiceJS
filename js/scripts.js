f202ef351e328abbc659ded3b6ace78478bcc1c6
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-04-15 14:24:22 -0700

    Front-end tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,34 @@ var Player = {
     // add score
     this.score += this.turnScore;
     // test for win
+    return this.score >= 100;
+  },
+
+  addTurnScore: function(score) {
+    this.turnScore += score;
   }
 };
 
 function roll() {
   // roll dice 1d6
+  return Math.floor(Math.random() * 6) + 1;
 };
 
 jQuery(document).ready(function() {
-  $("#").focus();
-  $("#").submit(function(event) {
+  $("#p1").focus();
+  $("#create-players").submit(function(event) {
     event.preventDefault();
+
+    var name1 = $("#p1").val();
+    var player1 = Object.create(Player);
+    player1.name = name1;
+
+    var name2 = $("#p2").val();
+    var player2 = Object.create(Player);
+    player2.name = name2;
+
+    $("#player-turn").text(player1.name + "'s turn");
+    $("#game").show();
 
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,23 @@ describe('PigDice', function() {
       expect(testPlayer.score).to.equal(5);
     });
   });
+
+  describe('addScore() winner', function() {
+    it("adds 100 to the player's score and returns winner", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.score = 1;
+      testPlayer.turnScore = 100;
+      var winner = testPlayer.addScore();
+      expect(winner).to.equal(true);
+    });
+  });
+
+  describe('addTurnScore()', function() {
+    it("adds 2 to the player's turn score", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.turnScore = 3;
+      testPlayer.addTurnScore(2);
+      expect(testPlayer.turnScore).to.equal(5);
+    });
+  });
 });
