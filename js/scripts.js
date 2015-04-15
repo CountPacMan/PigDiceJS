d5f53ba236c0ea55f015b9f4286f14bf5a26d929
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-04-15 14:41:24 -0700

    add dice functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ var Player = {
   }
 };
 
-function roll() {
+function getRoll() {
   // roll dice 1d6
   return Math.floor(Math.random() * 6) + 1;
 };
@@ -34,7 +34,21 @@ jQuery(document).ready(function() {
     player2.name = name2;
 
     $("#player-turn").text(player1.name + "'s turn");
+    $("#player-score").text(player1.score);
+    $("#player-turnScore").text(player1.turnScore);
     $("#game").show();
 
+    $("#roll").click(function(event) {
+      var roll = getRoll();
+      if (roll === 1) {
+        player1.turnScore = 0;
+        // end turn and give message
+      } else {
+        player1.addTurnScore(roll);
+        player1.addScore();
+      }
+      $("#player-score").text(player1.score);
+      $("#player-turnScore").text(player1.turnScore);
+    });
   });
 });
