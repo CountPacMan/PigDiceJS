d9c37f6f0a6f0ffe9627f61c454cd7e0b5315379
CountPacMan <venom20xx@yahoo.com>
2015-04-15 21:03:43 -0700

    fix new game functionality

--- js/scripts.js
+++ js/scripts.js
@@ -21,100 +21,88 @@ function getRoll() {
 };
 
 jQuery(document).ready(function() {
-  var name1;
-  var player1 = Object.create(Player);
+  $("#p1").focus();
+  $("#create-players").submit(function(event) {
+    event.preventDefault();
+    $("#create-players").hide();
 
-  var name2;
-  var player2 = Object.create(Player);
+    var name1 = $("#p1").val();
+    var player1 = Object.create(Player);
+    player1.name = name1;
+    player1.score = 0;
+    player1.turnScore = 0;
 
-  var currentPlayer = player1;
+    var name2 = $("#p2").val();
+    var player2 = Object.create(Player);
+    player2.name = name2;
+    player2.score = 0;
+    player2.turnScore = 0;
 
-  newGame();
-  function newGame() {
-    $("#p1").focus();
-    $("#create-players").submit(function(event) {
-      event.preventDefault();
-      $("#create-players").hide();
+    var currentPlayer = player1;
 
-      var name1 = $("#p1").val();
-      player1.name = name1;
-      player1.score = 0;
-      player1.turnScore = 0;
+    function printPlayer() {
+      $(".play").show();
+      $("#ok").hide();
+      $("#player-turn").text(currentPlayer.name + "'s turn");
+      $("#player-score").text(currentPlayer.score);
+      $("#player-turnScore").text(currentPlayer.turnScore);
+    }
 
-      var name2 = $("#p2").val();
-      player2.name = name2;
-      player2.score = 0;
-      player2.turnScore = 0;
-
-      var currentPlayer = player1;
-
-      function printPlayer() {
-        $(".play").show();
-        $("#ok").hide();
-        $("#player-turn").text(currentPlayer.name + "'s turn");
-        $("#player-score").text(currentPlayer.score);
-        $("#player-turnScore").text(currentPlayer.turnScore);
-      }
-
-      function changePlayer() {
-        currentPlayer.turnScore = 0;
-        $("#message").empty();
-        $("#player-score").text(currentPlayer.score);
-        $("#player-turnScore").text(currentPlayer.turnScore);
-        currentPlayer = currentPlayer === player1 ? player2 : player1;
-        printPlayer();
-      }
-
-      function updateScore() {
-        $("#player-score").text(currentPlayer.score);
-        $("#player-turnScore").text(currentPlayer.turnScore);
-      }
-
-      function winner() {
-        $("#game").hide();
-        $("#winner").text(currentPlayer.name + " is the winner!!!");
-        $("#end").show();
-      }
-
+    function changePlayer() {
+      currentPlayer.turnScore = 0;
+      $("#message").empty();
+      $("#player-score").text(currentPlayer.score);
+      $("#player-turnScore").text(currentPlayer.turnScore);
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
       printPlayer();
-      $("#game").show();
+    }
 
-      $("#roll").click(function(event) {
-        $("#message").text("");
-        var roll = getRoll();
-        if (roll === 1) {
-          $("#message").text("Rolled a 1! Lose your turn.").addClass("text-danger");
-          currentPlayer.turnScore = 0;
-          $(".play").hide();
-          $("#ok").show();
-        } else {
-          $("#message").text("Rolled a " + roll);
-          currentPlayer.addTurnScore(roll);
-          if (currentPlayer.turnScore + currentPlayer.score >= 10) {
-            winner();
-          }
-        }
-        updateScore();
-      });
+    function updateScore() {
+      $("#player-score").text(currentPlayer.score);
+      $("#player-turnScore").text(currentPlayer.turnScore);
+    }
 
-      $("#pass").click(function(event) {
-        currentPlayer.addScore();  // still need to test for win
-        updateScore();
-        $("#message").text("Adding turn score to total score...");
+    function winner() {
+      $("#game").hide();
+      $("#winner").text(currentPlayer.name + " is the winner!!!");
+      $("#end").show();
+    }
+
+    printPlayer();
+    $("#game").show();
+
+    $("#roll").click(function(event) {
+      $("#message").text("");
+      var roll = getRoll();
+      if (roll === 1) {
+        $("#message").text("Rolled a 1! Lose your turn.").addClass("text-danger");
+        currentPlayer.turnScore = 0;
         $(".play").hide();
         $("#ok").show();
-      });
-
-      $("#ok").click(function() {
-        changePlayer();
-      });
-
-      $("#new-game").click(function() {
-        $("#end").hide();
-        $("#create-players").show();
-        $("#message").empty();
-        newGame();
-      });
+      } else {
+        $("#message").text("Rolled a " + roll);
+        currentPlayer.addTurnScore(roll);
+        if (currentPlayer.turnScore + currentPlayer.score >= 10) {
+          winner();
+        }
+      }
+      updateScore();
     });
-  }
+
+    $("#pass").click(function(event) {
+      currentPlayer.addScore();  // still need to test for win
+      updateScore();
+      $("#message").text("Adding turn score to total score...");
+      $(".play").hide();
+      $("#ok").show();
+    });
+
+    $("#ok").click(function() {
+      changePlayer();
+    });
+
+    $("#new-game").click(function() {
+      location.reload();
+    });
+  });
 });
